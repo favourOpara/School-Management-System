550d4e1f3475a13001bbc56772a84cf115d8356e
favourOpara <favouropara48@gmail.com>
2025-11-26 16:00:30 +0000

    "Add PostgreSQL support"

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,13 +93,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+# Default to SQLite for local development
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
